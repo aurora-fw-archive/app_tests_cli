ac837a01ddbd4e652353fe85689ce9a47aadc14d
Ev1lbl0w <ricasubtil@gmail.com>
2017-09-01 13:19:23 +0100

    Updated copyrigth and links

--- src/test_shell.cpp
+++ src/test_shell.cpp
@@ -1,8 +1,8 @@
 // ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─ | Powerful, Scalable and Cross Platform Framework
 // ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐ | @author Luís Ferreira
 // ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴ | @license GNU Public License v3
-//  Copyright (c) 2016 - Luís Ferreira. All right reserved
-//  More information in: https://github.com/ljmf00/ (Github Page)
+//  Copyright (c) 2017 - Luís Ferreira. All right reserved
+//  More information in: https://github.com/aurora-fw/ (Github Page)
 
 // File: src/Examples/src/test_shell.cpp
 // Contains: Tests and Examples for Aurora Framework (Shell Example)
