d57d3ac1d674a63b432d444c92d2145b6105836c
Luís Ferreira <lsferreira169@gmail.com>
2017-09-03 00:20:17 +0100

    Making several code optimizations and project restructuration

--- app.cmake
+++ app.cmake
@@ -1,11 +1,27 @@
+# ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─
+# ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐
+# ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴
+# A Powerful General Purpose Framework
+# More information in: https://aurora-fw.github.io/
+#
+# Copyright (C) 2017 Aurora Framework, All rights reserved.
+#
+# This file is part of the Aurora Framework. This framework is free
+# software; you can redistribute it and/or modify it under the terms of
+# the GNU General Public License version 3 as published by the Free
+# Software Foundation and appearing in the file LICENSE included in the
+# packaging of this file. Please review the following information to
+# ensure the GNU General Public License version 3 requirements will be
+# met: https://www.gnu.org/licenses/gpl-3.0.html.
+
 message(STATUS "Loading tests-cli app...")
 
 if (NOT CONFIGURED_ONCE)
-	set(APP_TESTS_CLI_SOURCE_DIR ${APP_TESTS_CLI_DIR}/src)
+	set(AURORAFW_APP_TESTS_CLI_SOURCE_DIR ${AURORAFW_APP_TESTS_CLI_DIR}/src)
 endif()
 
-add_executable(aurorafw_app_tests_shell ${APP_TESTS_CLI_SOURCE_DIR}/test_shell.cpp)
+add_executable(aurorafw_app_tests_cli ${AURORAFW_APP_TESTS_CLI_SOURCE_DIR}/main.cpp)
 
-target_link_libraries(aurorafw_app_tests_shell aurorafw-core aurorafw-cli aurorafw-io)
+target_link_libraries(aurorafw_app_tests_cli aurorafw-core aurorafw-cli aurorafw-io)
 
-set_target_properties(aurorafw_app_tests_shell PROPERTIES OUTPUT_NAME test_shell.out)
+set_target_properties(aurorafw_app_tests_cli PROPERTIES OUTPUT_NAME aurorafw_app_tests_cli.out)
---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+/****************************************************************************
+** ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─
+** ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐
+** ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴
+** A Powerful General Purpose Framework
+** More information in: https://aurora-fw.github.io/
+**
+** Copyright (C) 2017 Aurora Framework, All rights reserved.
+**
+** This file is part of the Aurora Framework. This framework is free
+** software; you can redistribute it and/or modify it under the terms of
+** the GNU General Public License version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 3 requirements will be
+** met: https://www.gnu.org/licenses/gpl-3.0.html.
+****************************************************************************/
+
+#include <AuroraFW/Aurora.h>
+
+using namespace AuroraFW;
+
+Application *MyApp;
+
+ArSlot_t slot_MyApp_on_open()
+{
+	if(!Debug::getDebugStatus())
+	{
+		CLI::Log(CLI::Notice, "debug is disable");
+	}
+	if(Debug::getDebugStatus())
+	{
+		CLI::Log(CLI::Notice, "test the log");
+		CLI::Log(CLI::Warning, "test the log");
+		CLI::Log(CLI::Error, "test the log");
+		CLI::Log(CLI::Notice,"test the log");
+		CLI::Log(CLI::Debug, "test the log");
+	}
+	#ifdef AURORAFW_
+		CLI::Log(CLI::Notice, "Aurora Framework Macro detected!");
+	#endif
+
+	CLI::Log(CLI::Information, "Getting total virtual memory:\t\t", Info::RAM::getTotalVirtualMemory());
+	CLI::Log(CLI::Information, "Getting used virtual memory:\t\t", Info::RAM::getUsedVirtualMemory());
+	CLI::Log(CLI::Information, "Getting free virtual memory:\t\t", Info::RAM::getFreeVirtualMemory());
+	CLI::Log(CLI::Information, "Getting total physical memory:\t\t", Info::RAM::getTotalPhysicalMemory());
+	CLI::Log(CLI::Information, "Getting used physical memory:\t\t", Info::RAM::getUsedPhysicalMemory());
+	CLI::Log(CLI::Information, "Getting free physical memory:\t\t", Info::RAM::getFreePhysicalMemory());
+	CLI::Log(CLI::Information, "Getting Operation System Name:\t\t", Info::OS::getName());
+	CLI::Log(CLI::Information, "Getting Operation System Architecture:\t", Info::OS::getArchitecture());
+	CLI::Log(CLI::Information, "Getting Operation System Manufacturer:\t", Info::OS::getManufacturer());
+	CLI::Log(CLI::Information, "Getting Operation System Version:\t\t", Info::OS::getVersion());
+	CLI::Log(CLI::Information, "Getting User Computer Name:\t\t", Info::OS::getUserComputerName());
+
+	//Input testing
+	CLI::Output << "Insert something: ";
+	int hello;
+	CLI::Input >> hello;
+	CLI::Output << hello << CLI::EndLine;
+	Application::ExitSuccess();
+}
+
+int main(int argc, char * argv[])
+{
+	MyApp = new Application(slot_MyApp_on_open, argc, argv);
+	delete MyApp;
+	return 0;
+}
